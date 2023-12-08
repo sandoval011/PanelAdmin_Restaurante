3240dbd8865339bd59a2ea581a69be64b6ef0e22
Carmen Sandoval <carmen.sandoval@tecsup.edu.pe>
2023-12-07 22:15:25 -0500

    actualizado CRS

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -242,6 +242,8 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CELERY_BROKER_URL = "redis://localhost:6379/0"
