c099c40f220370bd1acc18ece5c7b83fd8d18837
Carmen Sandoval <carmen.sandoval@tecsup.edu.pe>
2023-12-07 21:20:22 -0500

    actualizado CRS

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -242,7 +242,7 @@ STATIC_URL = "/static/"
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
